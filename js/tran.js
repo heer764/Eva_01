9db9f6c039e60828cde4bc0d468b5238caacf2d3
heer764 <se.andrades@duocuc.cl>
2024-01-12 13:51:02 -0300

    arregle el inicio

--- js/tran.js
+++ js/tran.js
@@ -1,20 +1,22 @@
-function cambioimg (){
-    var imagenes = ["img/Denkion isotipo copia.png", "img/fondo_receiver.jpg", "img/fondo_tocadiscos.jpg"]
-    var i = 0;
-    var imagen = document.getElementById("imagenf");
+var i = 0;
+var imagenes = ["img/fondo_tocadiscos.jpg", "img/fondo_receiver.jpg", "img/fondo_radio.jpg"]
+var imagen = document.getElementById("imagenf");
 
-    setTimeout(function(){ 
-    imagen.style.opacity = 0;
+function cambioimg (){
+    imagen.style.opacity = 0.75;
 
     setTimeout(function(){
         imagen.src = imagenes[i];
-        imagen.style.opacity=1;
+        imagen.style.opacity = 1;
         i = (i+1) % imagenes.length;
-        cambioimg();         
-        }, 1000);
-    },3000);
+    },120);
+}
+
+function nuevaimg(){
+    cambioimg ()
+    setInterval(cambioimg,3000);
 }
 
 window.onload = function(){
-    cambioimg();
+    nuevaimg();
 };
